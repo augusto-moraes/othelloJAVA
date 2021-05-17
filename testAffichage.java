28cfb4902837ff06af1bd7403dc6275389b9459f
augusto-moraes <augui2009@gmail.com>
2021-05-17 14:37:11 +0200

    correction AffichageOthello CamelCase

--- testAffichage.java
+++ testAffichage.java
@@ -1,7 +1,7 @@
 public class testAffichage{
 	public static void main(String[] args){
 		int[][] tabEssai = {{0,0,0,2,0,1,0,0},{3,0,4,0,0,0,2,0},{0,0,0,1,3,0,0,0},{0,0,2,0,0,0,4,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0}};
-		affichageOthello jeu1 = new affichageOthello(tabEssai);
+		AffichageOthello jeu1 = new AffichageOthello(tabEssai);
 		jeu1.affichagePlateau();
 		MenuOthello menu1 = new MenuOthello();
 		menu1.affichageMenu();
