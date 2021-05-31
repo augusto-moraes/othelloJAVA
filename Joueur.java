9faefa8e8d5718d1eaffdad462c7bf36265c0273
augusto-moraes <augui2009@gmail.com>
2021-05-31 17:14:28 +0200

    Working isEnemyAround to poserPion condition

--- Joueur.java
+++ Joueur.java
@@ -1,11 +1,33 @@
 public class Joueur {
-    // Créer des joueurs
     private int couleur ;
     
     public Joueur (int couleur){
 		this.couleur = couleur ;
 	}
-	
-	Joueur joueurnoir = new Joueur (1); 
-	Joueur joueurblanc = new Joueur (2) ; 
+
+	public int getColor() {
+		return this.couleur;
+	}
+
+	public int getEnnemi() {
+        int res = 0 ;
+        
+        if (this.couleur == 1) {
+            res = 2 ; 
+        } else if (this.couleur == 2) {
+            res = 1 ; 
+        }
+        return res ; 
+    }
+
+	public String getName() {
+		String res = "undefined player";
+        
+        if (this.couleur == 1) {
+            res = "Noir"; 
+        } else if (this.couleur == 2) {
+            res = "Blanc"; 
+        }
+        return res ;
+	}
 }	 
